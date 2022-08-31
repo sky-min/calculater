9a878bc1c155e5d1c92cc198bc7af226dbb44679
skymin <81374952+sky-min@users.noreply.github.com>
2022-08-31 22:59:36 +0900

    Update calculator.js

--- js/calculator.js
+++ js/calculator.js
@@ -1,26 +1,21 @@
-var oEle1 = document.getElementById('display') ;
-var oEle2 = document.getElementById('result') ;
+const Display = document.getElementById('display') ;
+const Result = document.getElementById('result') ;
 
 function add(char) {
-    var display = document.getElementById('display'); 
-    display.value = display.value + char;
+    const before = Display.value;
+    Display.value = before + char;
 }
 
 function onResult() {
-  var display = document.getElementById('display');
-  var result = eval(display.value);
-  document.getElementById('result').value = result;
+    Result.value = eval(Display.value);
 }
 
 function reset() {
-    document.getElementById('display').value = "";
-    document.getElementById('result').value = "";
+    Display.value = "";
+    Result.value = "";
 }
 
 function back(){
-  var exp = document.getElementById('display').value;
-  document.getElementById('display').value = exp.substring(0,exp.length - 1);
+    const before = Display.value;
+    Display.value = exp.substring(0, before.length - 1);
 }
-
-oEle1.readOnly = true ;
-oEle2.readOnly = true ;
